3dbd4c33abf27733018ce6b9fccee284d6532731
srinathchettiar07 <srinathchettiaar786@gmail.com>
2025-06-09 22:01:51 +0530

    add

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,7 +18,7 @@ app.use(cors({
     origin:"http://localhost:5173",
     credentials: true,
 }))
-const __dirbane = path.resolve();
+const __dirname = path.resolve();
 
 
 app.use("/api/auth" , authRoutes) ;
